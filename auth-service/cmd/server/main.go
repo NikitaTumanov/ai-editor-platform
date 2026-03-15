b02b14699dc0befced402f5e82358e8fa0e6e5ba
Nikita Tumanov <toomuchick@gmail.com>
2026-03-15 18:15:53 +0300

    add repo and serv for storage-service

--- auth-service/cmd/server/main.go
+++ auth-service/cmd/server/main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net"
 
-	authpb "github.com/NikitaTumanov/ai-editor-platform/protos/auth"
+	grpcauth "github.com/NikitaTumanov/ai-editor-platform/auth-service/internal/grpc"
+	authpb "github.com/NikitaTumanov/ai-editor-platform/protos/auth_service"
 	"google.golang.org/grpc"
 )
-
-type authServer struct {
-	authpb.UnimplementedAuthServer
-}
-
-func (s *authServer) Login(_ context.Context, in *authpb.LoginRequest) (*authpb.LoginResponse, error) {
-	return &authpb.LoginResponse{Token: "asacac"}, nil
-}
-
-func (s *authServer) Register(_ context.Context, in *authpb.RegisterRequest) (*authpb.RegisterResponse, error) {
-	return &authpb.RegisterResponse{UserId: 1}, nil
-}
 
 func main() {
 	lis, err := net.Listen("tcp", ":8030")
@@ -30,6 +18,6 @@ func main() {
 	var opts []grpc.ServerOption
 
 	grpcServer := grpc.NewServer(opts...)
-	authpb.RegisterAuthServer(grpcServer, &authServer{})
+	authpb.RegisterAuthServer(grpcServer, grpcauth.NewAuthServer())
 	grpcServer.Serve(lis)
 }
